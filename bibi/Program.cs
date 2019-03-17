097d43c2ecc2817a697cb98e18451a2b98ff54b2
agogolan <gogolan.adrian@gmail.com>
2019-03-17 20:14:25 +0200

    Added main loop and string generation

--- bibi/Program.cs
+++ bibi/Program.cs
@@ -1,13 +1,54 @@
 using System;
+using System.Text;
 
 namespace bibi
 {
 	class MainClass
 	{
+		static Random random = new Random ();
+		static StringBuilder sb = new StringBuilder ();
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Watch carefully then repeat the string!");
 			Console.WriteLine ("Press any key to start!");
+			Console.ReadKey ();
+
+			bool success = false;
+			string toRepeat = null;
+			string userInput = null;
+			int score = 1;
+
+			do
+			{
+				toRepeat = GenerateString(score);
+				OutputString(toRepeat);
+				userInput = GetInputString();
+				success = toRepeat == userInput;
+				score++;
+			}
+			while(success);
+		}
+
+		static string GenerateString (int length)
+		{
+			sb.Clear ();
+			for (int i = 0; i < length; i++)
+			{
+				sb.Append (random.Next (0, 2));
+			}
+
+			return sb.ToString();
+		}
+
+		static void OutputString (string toOutput)
+		{
+			Console.WriteLine (toOutput);
+		}
+
+		static string GetInputString ()
+		{
+			return Console.ReadLine ();
 		}
 	}
 }
