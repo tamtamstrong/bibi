6a71910b0028c8f3988d71aa29ef15aad38bcd6c
agogolan <gogolan.adrian@gmail.com>
2019-03-17 19:41:49 +0200

    Added new project.

---
+++ bibi/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace bibi
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Watch carefully then repeat the string!");
+			Console.WriteLine ("Press any key to start!");
+		}
+	}
+}
