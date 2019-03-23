e1215fcc6bd93fd4b787c606706ea963df902abb
agogolan <gogolan.adrian@gmail.com>
2019-03-23 14:16:14 +0200

    Added implementation

--- bibi/Program.cs
+++ bibi/Program.cs
@@ -7,12 +7,16 @@ namespace bibi
 	{
 		static Random random = new Random ();
 		static StringBuilder sb = new StringBuilder ();
+		static int difficultyLevel = 0;
+		static int displayTimeInMilliseconds = 300;
 
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Watch carefully then repeat the string!");
+			SetDifficulty ();
 			Console.WriteLine ("Press any key to start!");
 			Console.ReadKey ();
+			Console.Clear ();
 
 			bool success = false;
 			string toRepeat = null;
@@ -28,6 +32,27 @@ namespace bibi
 				score++;
 			}
 			while(success);
+
+			Console.WriteLine (string.Format ("You scored {0} on a difficulty level of {1}", score, difficultyLevel));
+		}
+
+		static void SetDifficulty ()
+		{
+			Console.WriteLine ("Choose difficulty level (1 = easiest, 5 = hardest):");
+			bool difficultyWasSet = false;
+			do
+			{
+				string input = Console.ReadLine ();
+				if (int.TryParse(input, out difficultyLevel) && difficultyLevel >= 1 && difficultyLevel <= 5)
+				{
+					difficultyWasSet = true;
+				}
+				else
+				{
+					Console.WriteLine("You must enter an integer between 1 and 5!");
+				}
+			}
+			while (!difficultyWasSet);
 		}
 
 		static string GenerateString (int length)
@@ -43,12 +68,26 @@ namespace bibi
 
 		static void OutputString (string toOutput)
 		{
+			switch(difficultyLevel)
+			{
+				case 1: displayTimeInMilliseconds = 2000; break;
+				case 2: displayTimeInMilliseconds = 1500; break;
+				case 3: displayTimeInMilliseconds = 1000; break;
+				case 4: displayTimeInMilliseconds = 500; break;
+				case 5: displayTimeInMilliseconds = 200; break;
+				default: displayTimeInMilliseconds = 1000; break;
+			};
+
 			Console.WriteLine (toOutput);
+			System.Threading.Thread.Sleep (displayTimeInMilliseconds);
+			Console.Clear ();
 		}
 
 		static string GetInputString ()
 		{
-			return Console.ReadLine ();
+			string input = Console.ReadLine ();
+			Console.Clear ();
+			return input;
 		}
 	}
 }
